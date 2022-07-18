428afe025ddff64294bc4b476791f20329571da5
Matheus <stvgamer74@gmail.com>
2022-07-18 15:38:53 -0300

    feat: criando migrate de blog posts

---
+++ src/database/migrations/20220718182032-create-blog-posts.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'userId',
+        references: {
+          model: 'Users',
+          key: 'id',
+        }
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('BlogPosts');
+  }
+};
